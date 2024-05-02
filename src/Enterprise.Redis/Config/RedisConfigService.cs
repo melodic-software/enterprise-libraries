964e802c467bc29484b7c23dc49722a29be2cc19
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-01 19:00:36 -0500

    Update caching package, add distributed in memory cache fallback (dev only)

--- src/Enterprise.Redis/Config/RedisConfigService.cs
+++ src/Enterprise.Redis/Config/RedisConfigService.cs
@@ -1,16 +1,20 @@
-﻿using Enterprise.Exceptions;
+﻿using System;
+using Enterprise.Exceptions;
+using Enterprise.Logging.Core.Loggers;
 using Enterprise.Options.Core.Singleton;
 using Enterprise.Redis.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 
 namespace Enterprise.Redis.Config;
 
 public static class RedisConfigService
 {
-    public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+    public static void ConfigureRedis(this IServiceCollection services, IHostEnvironment environment, IConfiguration configuration)
     {
         RedisConfigOptions configOptions = OptionsInstanceService.Instance
             .GetOptionsInstance<RedisConfigOptions>(configuration, RedisConfigOptions.ConfigSectionKey);
@@ -27,18 +31,36 @@ public static class RedisConfigService
                 "Please ensure the connection string name is accurate and that a value has been provided."
             );
         }
-            
-        // This is a connection to our Redis interface.
-        // We register it as a singleton because it will be resolved internally by some services we'll be using.
-        // TODO: use async version of this?
-        IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
-        services.TryAddSingleton(connectionMultiplexer);
 
-        services.AddStackExchangeRedisCache(options =>
+        try
         {
-            //options.Configuration = redisConnectionString;
-            //options.InstanceName = configOptions.RedisInstanceName;
-            options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
-        });
+            // This is a connection to our Redis interface.
+            // We register it as a singleton because it will be resolved internally by some services we'll be using.
+            // TODO: use async version of this?
+            IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+            services.TryAddSingleton(connectionMultiplexer);
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                //options.Configuration = redisConnectionString;
+                //options.InstanceName = configOptions.RedisInstanceName;
+                options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
+            });
+        }
+        catch (Exception ex)
+        {
+            // This could happen if we run migrations, in which case a local Redis container likely won't be running.
+            PreStartupLogger.Instance.LogError(ex, "Redis could not be registered.");
+
+            if (!environment.IsDevelopment())
+            {
+                // If this fails in a non development environment, we need to fail fast.
+                throw;
+            }
+
+            // We don't want to use a fallback unless we're running locally.
+            PreStartupLogger.Instance.LogWarning("Falling back to the distributed memory cache.");
+            services.AddDistributedMemoryCache();
+        }
     }
 }
