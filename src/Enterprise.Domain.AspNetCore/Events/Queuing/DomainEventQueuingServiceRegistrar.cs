bf4863b05da85766fe16828f0db00b4fa602465c
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-07 17:51:29 -0500

    Update package, simplify registrations

--- src/Enterprise.Domain.AspNetCore/Events/Queuing/DomainEventQueuingServiceRegistrar.cs
+++ src/Enterprise.Domain.AspNetCore/Events/Queuing/DomainEventQueuingServiceRegistrar.cs
@@ -1,9 +1,6 @@
-﻿using Enterprise.DesignPatterns.Decorator.Services.Abstract;
-using Enterprise.DI.Core.Registration;
-using Enterprise.Domain.AspNetCore.Events.Queuing.Decoration;
+﻿using Enterprise.DI.Core.Registration;
 using Enterprise.Domain.Events.Queuing;
-using Enterprise.Domain.Events.Raising;
-using Enterprise.Events.Callbacks.Raising.Abstract;
+using Enterprise.Domain.Events.Queuing.Options;
 using Enterprise.Options.Core.Singleton;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -17,8 +14,8 @@ internal class DomainEventQueuingServiceRegistrar : IRegisterServices
 {
     public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
     {
-        DomainEventQueueConfigOptions configOptions = OptionsInstanceService.Instance
-            .GetOptionsInstance<DomainEventQueueConfigOptions>(configuration, DomainEventQueueConfigOptions.ConfigSectionKey);
+        DomainEventQueuingConfigOptions configOptions = OptionsInstanceService.Instance
+            .GetOptionsInstance<DomainEventQueuingConfigOptions>(configuration, DomainEventQueuingConfigOptions.ConfigSectionKey);
 
         if (!configOptions.EnableDomainEventQueuing)
             return;
@@ -30,23 +27,5 @@ internal class DomainEventQueuingServiceRegistrar : IRegisterServices
             ILogger<HttpContextDomainEventQueueService> logger = provider.GetRequiredService<ILogger<HttpContextDomainEventQueueService>>();
             return new HttpContextDomainEventQueueService(httpContextAccessor, logger);
         });
-
-        // We're replacing any existing registrations, and are replacing it with a queue based implementation. 
-        services.RemoveAll(typeof(IRaiseDomainEvents));
-
-        // We have to use scoped here because the dependency being injected in is scoped,
-        // and would cause problems if we registered this with a singleton lifetime.
-        services.BeginRegistration<IRaiseDomainEvents>()
-            .AddScoped(provider =>
-            {
-                IEnqueueDomainEvents domainEventQueueService = provider.GetRequiredService<IEnqueueDomainEvents>();
-                return new QueueingDomainEventRaiser(domainEventQueueService);
-            })
-            .WithDecorator((provider, eventRaiser) =>
-            {
-                IGetDecoratedInstance decoratorService = provider.GetRequiredService<IGetDecoratedInstance>();
-                IRaiseEventCallbacks callbackRaiser = provider.GetRequiredService<IRaiseEventCallbacks>();
-                return new DomainEventCallbackRaisingDecorator(eventRaiser, decoratorService, callbackRaiser);
-            });
     }
 }
