213268118e92373dd815ee3bee3d10e726d71687
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-15 13:49:38 -0500

    Add multitenancy project

---
+++ src/Enterprise.Multitenancy/Options/MultitenancyConfigOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Enterprise.Multitenancy.Options;
+
+public class MultitenancyConfigOptions
+{
+    public const string ConfigSectionKey = "Custom:Multitenancy";
+
+    /// <summary>
+    /// Determines if multi tenancy related services are registered.
+    /// If this is not enabled, the application will fail when requests for any of these services are made.
+    /// </summary>
+    public bool MultiTenancyEnabled { get; set; } = false;
+
+    /// <summary>
+    /// Determines if a <see cref="Exceptions.TenantIdNotFoundException"/> will be thrown if the tenant ID cannot be located (when requested).
+    /// </summary>
+    public bool TenantIdRequired { get; set; } = false;
+}
