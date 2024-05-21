11577279fcd80b1cee3c6749d8a9779b699f86c3
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-21 11:04:01 -0500

    Update interfaces

--- src/Enterprise.ApplicationServices.Core/Commands/Handlers/IResolveCommandHandler.cs
+++ src/Enterprise.ApplicationServices.Core/Commands/Handlers/IResolveCommandHandler.cs
@@ -8,8 +8,17 @@ public interface IResolveCommandHandler
     /// <summary>
     /// Get the handler implementation that can handle the given command.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TCommand"></typeparam>
     /// <param name="command"></param>
     /// <returns></returns>
-    IHandleCommand<T> GetHandlerFor<T>(T command) where T : IBaseCommand;
+    IHandleCommand<TCommand> GetHandlerFor<TCommand>(TCommand command) where TCommand : IBaseCommand;
+
+    /// <summary>
+    /// Get the handler implementation that can handle the given command.
+    /// </summary>
+    /// <typeparam name="TCommand"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    IHandleCommand<TCommand, TResponse> GetHandlerFor<TCommand, TResponse>(TCommand command) where TCommand : IBaseCommand;
 }
