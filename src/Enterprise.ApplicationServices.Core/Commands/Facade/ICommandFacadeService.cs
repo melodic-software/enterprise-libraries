4d34559dcc1623a8743137fec67d922276344541
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-22 10:55:45 -0500

    Update facade interface

--- src/Enterprise.ApplicationServices.Core/Commands/Facade/ICommandFacadeService.cs
+++ src/Enterprise.ApplicationServices.Core/Commands/Facade/ICommandFacadeService.cs
@@ -1,6 +1,6 @@
 ﻿using Enterprise.ApplicationServices.Core.Commands.Dispatching;
-using Enterprise.Events.Callbacks.Facade.Abstractions;
+using Enterprise.Events.Callbacks.Registration.Abstract;
 
 namespace Enterprise.ApplicationServices.Core.Commands.Facade;
 
-public interface ICommandFacadeService : IDispatchCommands, IEventCallbackService;
+public interface ICommandFacadeService : IDispatchCommands, IRegisterEventCallbacks, IClearCallbacks;
