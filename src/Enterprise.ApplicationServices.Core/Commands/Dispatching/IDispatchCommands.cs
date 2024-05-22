adcb2a72a64736e48642a05e99d2dbc79882beb7
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-22 10:25:45 -0500

    Update interface

--- src/Enterprise.ApplicationServices.Core/Commands/Dispatching/IDispatchCommands.cs
+++ src/Enterprise.ApplicationServices.Core/Commands/Dispatching/IDispatchCommands.cs
@@ -3,7 +3,7 @@ using Enterprise.Events.Callbacks.Registration.Abstract;
 
 namespace Enterprise.ApplicationServices.Core.Commands.Dispatching;
 
-public interface IDispatchCommands : IRegisterEventCallbacks
+public interface IDispatchCommands : IRegisterEventCallbacks, IClearCallbacks
 {
     Task DispatchAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
         where TCommand : IBaseCommand;
