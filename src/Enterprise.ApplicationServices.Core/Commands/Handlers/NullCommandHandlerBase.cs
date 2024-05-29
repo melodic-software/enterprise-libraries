04ecd2c6cffd6990871767b756e30f5bd6934152
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-29 16:41:39 -0500

    Add base classes

---
+++ src/Enterprise.ApplicationServices.Core/Commands/Handlers/NullCommandHandlerBase.cs
@@ -0,0 +1,27 @@
+﻿using Enterprise.ApplicationServices.Core.Commands.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Enterprise.ApplicationServices.Core.Commands.Handlers;
+
+public abstract class NullCommandHandlerBase
+{
+    protected ILogger Logger { get; }
+
+    protected NullCommandHandlerBase(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    protected void LogWarning(ICommand command)
+    {
+        string handlerTypeName = GetType().Name;
+        string commandTypeName = command.GetType().Name;
+
+        Logger.LogWarning(
+            "The {HandlerType} has been sent a \"{CommandType}\" command. " +
+            "The command will not be handled. " +
+            "Please verify command handler registrations.",
+            handlerTypeName, commandTypeName
+        );
+    }
+}
---
+++ src/Enterprise.ApplicationServices.Core/Queries/Handlers/NullQueryHandlerBase.cs
@@ -0,0 +1,27 @@
+﻿using Enterprise.ApplicationServices.Core.Queries.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Enterprise.ApplicationServices.Core.Queries.Handlers;
+
+public abstract class NullQueryHandlerBase
+{
+    protected readonly ILogger Logger;
+
+    protected NullQueryHandlerBase(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    protected void LogWarning(IQuery query)
+    {
+        string handlerTypeName = GetType().Name;
+        string queryTypeName = query.GetType().Name;
+
+        Logger.LogWarning(
+            "The {HandlerType} has been sent a \"{QueryType}\" query. " +
+            "The query will not be handled. " +
+            "Please verify query handler registrations.",
+            handlerTypeName, queryTypeName
+        );
+    }
+}
