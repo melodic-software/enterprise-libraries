3a605517d709ddcb4c04ad2a9b3c4cec8c5f1742
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-05-29 21:01:53 -0500

    Remove exceptions

--- src/Enterprise.ApplicationServices/Queries/Dispatching/QueryDispatcher.cs
+++ src/Enterprise.ApplicationServices/Queries/Dispatching/QueryDispatcher.cs
@@ -19,18 +19,7 @@ public class QueryDispatcher : IDispatchQueries
     public async Task<Result<TResponse>> DispatchAsync<TResponse>(IQuery query, CancellationToken cancellationToken)
     {
         IHandleQuery<TResponse> handler = _queryHandlerResolver.GetQueryHandler<TResponse>(query);
-
-        if (handler == null)
-        {
-            throw new InvalidOperationException(
-                $"An implementation of {nameof(IHandleQuery<TResponse>)} " +
-                $"could not be resolved for query \"{query.GetType().FullName}\" " +
-                $"with \"{typeof(TResponse)}\" response."
-            );
-        }
-
         Result<TResponse> result = await handler.HandleAsync(query, cancellationToken);
-
         return result;
     }
 
@@ -38,18 +27,7 @@ public class QueryDispatcher : IDispatchQueries
     public async Task<Result<TResponse>> DispatchAsync<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken)
     {
         IHandleQuery<TResponse> handler = _queryHandlerResolver.GetQueryHandler(query);
-
-        if (handler == null)
-        {
-            throw new InvalidOperationException(
-                $"An implementation of {nameof(IHandleQuery<TResponse>)} " +
-                $"could not be resolved for query \"{query.GetType().FullName}\" " +
-                $"with \"{typeof(TResponse)}\" response."
-            );
-        }
-
         Result<TResponse> result = await handler.HandleAsync(query, cancellationToken);
-
         return result;
     }
 
@@ -57,18 +35,7 @@ public class QueryDispatcher : IDispatchQueries
     public async Task<Result<TResponse>> DispatchAsync<TQuery, TResponse>(TQuery query, CancellationToken cancellationToken) where TQuery : IQuery
     {
         IHandleQuery<TQuery, TResponse> handler = _queryHandlerResolver.GetQueryHandler<TQuery, TResponse>(query);
-
-        if (handler == null)
-        {
-            throw new InvalidOperationException(
-                $"An implementation of {nameof(IHandleQuery<TQuery, TResponse>)} " +
-                $"could not be resolved for query \"{typeof(TQuery).FullName}\" " +
-                $"with \"{typeof(TResponse)}\" response."
-            );
-        }
-
         Result<TResponse> result = await handler.HandleAsync(query, cancellationToken);
-
         return result;
     }
 }
