816871c22b1e9fa04acf71dc55315978391a8cc9
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-06-04 17:30:32 -0500

    Revert change

--- src/Enterprise.ApplicationServices.Core/Commands/Dispatching/IDispatchCommands.cs
+++ src/Enterprise.ApplicationServices.Core/Commands/Dispatching/IDispatchCommands.cs
@@ -1,9 +1,13 @@
 ﻿using Enterprise.ApplicationServices.Core.Commands.Model;
+using Enterprise.ApplicationServices.Core.Commands.Model.Alternate;
 
 namespace Enterprise.ApplicationServices.Core.Commands.Dispatching;
 
-public interface IDispatchBaseCommands
+public interface IDispatchCommands
 {
     Task DispatchAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
         where TCommand : IBaseCommand;
+
+    Task<TResponse> DispatchAsync<TCommand, TResponse>(TCommand command, CancellationToken cancellationToken)
+        where TCommand : ICommand<TResponse>;
 }
--- src/Enterprise.ApplicationServices.Core/Commands/Dispatching/IDispatchCommandsWithResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Enterprise.ApplicationServices.Core.Commands.Model.Alternate;
-
-namespace Enterprise.ApplicationServices.Core.Commands.Dispatching;
-
-public interface IDispatchCommandsWithResponse : IDispatchCommandsWithResult
-{
-    Task<TResponse> DispatchAsync<TCommand, TResponse>(TCommand command, CancellationToken cancellationToken)
-        where TCommand : ICommand<TResponse>;
-}
