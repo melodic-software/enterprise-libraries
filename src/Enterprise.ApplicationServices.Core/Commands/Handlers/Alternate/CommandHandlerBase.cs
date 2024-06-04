526e8de8c38e9b8fcece09fbbbda3eae6470149e
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-06-04 15:50:15 -0500

    Chain base classes

--- src/Enterprise.ApplicationServices.Core/Commands/Handlers/Alternate/CommandHandlerBase.cs
+++ src/Enterprise.ApplicationServices.Core/Commands/Handlers/Alternate/CommandHandlerBase.cs
@@ -1,15 +1,12 @@
-﻿using Enterprise.ApplicationServices.Core.Commands.Model;
-using Enterprise.ApplicationServices.Core.Commands.Model.Alternate;
-using Enterprise.ApplicationServices.Core.Standard;
+﻿using Enterprise.ApplicationServices.Core.Commands.Model.Alternate;
 using Enterprise.DesignPatterns.ChainOfResponsibility.Pipeline.Delegates;
 using Enterprise.DesignPatterns.ChainOfResponsibility.Pipeline.Handlers;
 using Enterprise.Events.Facade.Abstract;
-using static Enterprise.ApplicationServices.Core.Commands.Handlers.Validation.CommandHandlerTypeValidationService;
 
 namespace Enterprise.ApplicationServices.Core.Commands.Handlers.Alternate;
 
 public abstract class CommandHandlerBase<TCommand, TResponse>
-    : ApplicationServiceBase, IHandleCommand<TCommand, TResponse>, IHandler<TCommand, TResponse>
+    : CommandHandlerBase<TCommand>, IHandleCommand<TCommand, TResponse>, IHandler<TCommand, TResponse>
     where TCommand : ICommand<TResponse>
 {
     protected CommandHandlerBase(IEventRaisingFacade eventService) : base(eventService)
@@ -23,19 +20,10 @@ public abstract class CommandHandlerBase<TCommand, TResponse>
     }
 
     /// <inheritdoc />
-    public async Task HandleAsync(IBaseCommand command, CancellationToken cancellationToken)
-    {
-        ValidateType(command, this);
-        var typedCommand = (TCommand)command;
-        await HandleAsync(typedCommand, cancellationToken);
-    }
-
-    /// <inheritdoc />
     public async Task<TResponse?> HandleAsync(TCommand request, SuccessorDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         return await HandleAsync(request, cancellationToken);
     }
 
-    /// <inheritdoc />
-    public abstract Task<TResponse> HandleAsync(TCommand command, CancellationToken cancellationToken);
+    public abstract override Task<TResponse> HandleAsync(TCommand command, CancellationToken cancellationToken);
 }
