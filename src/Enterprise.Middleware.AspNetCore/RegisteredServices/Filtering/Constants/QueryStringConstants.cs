b7a714321d3470931a5f5b56d166d7c17deb4390
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-06-15 15:31:33 -0500

    Organize alphabetically

--- src/Enterprise.Middleware.AspNetCore/RegisteredServices/Filtering/Constants/QueryStringConstants.cs
+++ src/Enterprise.Middleware.AspNetCore/RegisteredServices/Filtering/Constants/QueryStringConstants.cs
@@ -2,10 +2,10 @@
 
 public static class QueryStringConstants
 {
-    public const string ServiceLifetimes = "serviceLifetimes";
-    public const string ServiceTypes = "serviceTypes";
-    public const string ServiceKeys = "serviceKeys";
     public const string ExcludeMicrosoft = "excludeMicrosoft";
-    public const string Namespaces = "namespaces";
     public const string GroupByNamespace = "groupByNamespace";
+    public const string Namespaces = "namespaces";
+    public const string ServiceLifetimes = "serviceLifetimes";
+    public const string ServiceKeys = "serviceKeys";
+    public const string ServiceTypes = "serviceTypes";
 }
