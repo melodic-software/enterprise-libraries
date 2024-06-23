d8270aa753a7155016c87031c0c67ebba9b45a88
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-06-23 11:50:16 -0500

    Update projects

--- src/Enterprise.Testing.AspNetCore/Services/HttpContextCreationService.cs
+++ src/Enterprise.Testing.AspNetCore/Services/HttpContextCreationService.cs
@@ -8,7 +8,7 @@ public static class HttpContextCreationService
     /// <summary>
     /// Use this when you need an HttpContext instance, but you don't need to change any of its read-only properties.
     /// Some of the read-only properties include the features collection, connection, http request and response objects, etc.
-    /// In the case that you do need to change these, use Moq for mocking an HttpContext instance.
+    /// In the case that you do need to change these, use NSubstitute for mocking an HttpContext instance.
     /// </summary>
     /// <returns></returns>
     public static HttpContext CreateDefaultContext() => new DefaultHttpContext();
