48dc45aaf6a75e4f5f327333c28f91186f3948af
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-06-30 14:04:37 -0500

    Update packages, implement module logging

--- src/Enterprise.MediatR.Behaviors/Logging/RequestLoggingBehavior.cs
+++ src/Enterprise.MediatR.Behaviors/Logging/RequestLoggingBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Enterprise.ModularMonoliths.ModuleNaming;
 using Enterprise.Patterns.ResultPattern.Model;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -10,12 +11,15 @@ public class RequestLoggingBehavior<TRequest, TResult> :
     IPipelineBehavior<TRequest, TResult>
     where TRequest : notnull
 {
-    private const string Errors = "Errors";
+    private const string ErrorsPropertyName = "Errors";
+    private const string ModulePropertyName = "Module";
     private readonly ILogger<RequestLoggingBehavior<TRequest, TResult>> _logger;
+    private readonly IModuleNameService _moduleNameService;
 
-    public RequestLoggingBehavior(ILogger<RequestLoggingBehavior<TRequest, TResult>> logger)
+    public RequestLoggingBehavior(ILogger<RequestLoggingBehavior<TRequest, TResult>> logger, IModuleNameService moduleNameService)
     {
         _logger = logger;
+        _moduleNameService = moduleNameService;
     }
 
     public async Task<TResult> Handle(TRequest request,
@@ -27,18 +31,25 @@ public class RequestLoggingBehavior<TRequest, TResult> :
         Type requestType = request.GetType();
         string requestTypeName = requestType.Name;
 
+        // If the application is configured as a modular monolith, 
+        // the name will be returned based on a specific convention.
+        string moduleName = _moduleNameService.GetModuleName(requestType);
+
+        using IDisposable? moduleLogContext = !string.IsNullOrWhiteSpace(moduleName) ? 
+            LogContext.PushProperty(ModulePropertyName, moduleName) : 
+            null;
+        
+        using IDisposable loggerScope = _logger.BeginScope("{@Request}", request);
+
         var stopWatch = Stopwatch.StartNew();
 
-        using IDisposable scope = _logger.BeginScope("{@Request}", request);
-
-        _logger.LogInformation("Executing request.");
+        _logger.LogInformation("Processing request.");
         TResult result = await next();
         HandleResult(result);
 
         stopWatch.Stop();
 
-        _logger.LogInformation("Handled \"{Request}\" in {Milliseconds} ms.",
-            requestTypeName, stopWatch.ElapsedMilliseconds);
+        _logger.LogInformation("Request completed in {Milliseconds} ms.", stopWatch.ElapsedMilliseconds);
 
         return result;
     }
@@ -47,24 +58,26 @@ public class RequestLoggingBehavior<TRequest, TResult> :
     {
         if (genericResult is not Result result)
         {
-            _logger.LogInformation("Request execution completed.");
+            _logger.LogInformation("Request completed.");
             return;
         }
 
         if (result.IsSuccess)
         {
-            _logger.LogInformation("Request was successful.");
+            _logger.LogInformation("Request completed successfully.");
         }
         else
         {
-            var propertyValue = result.Errors.Select(x => new { x.Code, x.Message }).ToList();
+            var propertyValue = result.Errors
+                .Select(x => new { x.Code, x.Message })
+                .ToList();
 
             // https://github.com/serilog/serilog/wiki/Enrichment
             // This is a form of log enrichment that acts similarly to logging scopes.
             // It requires the log context enricher to be enabled in the logger configuration.
-            using (LogContext.PushProperty(Errors, propertyValue, true))
+            using (LogContext.PushProperty(ErrorsPropertyName, propertyValue, true))
             {
-                _logger.LogInformation("Request failed with errors. {@Errors}", result.Errors);
+                _logger.LogInformation("Request failed with error(s). {@Errors}", result.Errors);
             }
         }
     }
